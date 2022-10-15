a3e8c2c5143566f9bdcfedd78fd192130390ea6f
Suraj Anumandla <rajsuraj9262gmail.com>
2022-10-15 15:48:18 +0530

    Pizza Billing

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,8 +3,8 @@ package com.driver;
 public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        addExtraCheese();
-        addExtraToppings();
+        this.addExtraCheese();
+        this.addExtraToppings();
         // your code goes here
 
 
